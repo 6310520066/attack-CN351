5c32bde4fc680967cf6a437ebf3d84120974d8b7
6310520066 <6310520066@student.tu.ac.th>
2023-05-29 21:21:56 +0700

    fix some issue

--- webroot/index.php
+++ webroot/index.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 
 $mysqli = require __DIR__ . "/database.php";
@@ -18,33 +17,26 @@ if (isset($_SESSION["user_id"])) {
     $result = $mysqli->query($sql);
     $user = $result->fetch_assoc();
 }
-
-// Handle blog post submission
-if (isset($_POST["header"]) && isset($_POST["detail"]) && isset($_SESSION["user_id"])) {
-    $header = $_POST["header"];
-    $detail = $_POST["detail"];
-
-    // Insert the new blog post into the database
-    $sql = "INSERT INTO blog (header, detail, created_by, created_at)
-            VALUES ('$header', '$detail', $user_id, CURRENT_TIMESTAMP)";
-    $mysqli->query($sql);
-
-    // Redirect to the homepage
-    header("Location: index.php");
-    exit();
-}
-
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Home</title>
     <meta charset="UTF-8">
+    <style>
+        /* Add CSS styles */
+        .blog {
+            background-color: lightgrey;
+            border-radius: 10px;
+            padding: 10px;
+            margin-bottom: 10px;
+        }
+    </style>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
 </head>
 <body>
 
-<h1>Home</h1>
+<h1>BLOGGER</h1>
 
 <?php if (isset($user)): ?>
 
@@ -62,16 +54,13 @@ if (isset($_POST["header"]) && isset($_POST["detail"]) && isset($_SESSION["user_
 <h2>Blog Posts</h2>
 
 <?php if (!empty($blogs)): ?>
-    <ul>
-        <?php foreach ($blogs as $blog): ?>
-            <li>
-                <h3><?= htmlspecialchars($blog["header"]) ?></h3>
-                <p><?= htmlspecialchars($blog["detail"]) ?></p>
-                <p>Created by: <?= htmlspecialchars($blog["created_by"]) ?></p>
-                <p>Created at: <?= htmlspecialchars($blog["created_at"]) ?></p>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+    <?php foreach ($blogs as $blog): ?>
+        <div class="blog">
+            <h3><?= htmlspecialchars($blog["header"]) ?></h3>
+            <p><?= htmlspecialchars($blog["detail"]) ?></p>
+            <p style="font-size: small">Created by: <?= htmlspecialchars($blog["created_by"]) ?>, created at:<?= htmlspecialchars($blog["created_at"]) ?></p>
+        </div>
+    <?php endforeach; ?>
 <?php else: ?>
     <p>No blog posts found.</p>
 <?php endif; ?>
